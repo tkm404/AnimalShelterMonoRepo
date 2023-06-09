edcf90162abfc4e95730403adb5412a912b821ef
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-09 08:30:56 -0700

    Add animal model.

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelterApi.Models
+{
+  public class AnimalShelterApi
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Sepcies { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+  }
+}
